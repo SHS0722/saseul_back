5d8f921179c04cf867092843c016a9fbd129c075
songhs0718@gmail.com <songhs0718@gmail.com>
2024-04-20 01:47:59 +0900

    ip-test4

--- src/ip/ip.repository.ts
+++ src/ip/ip.repository.ts
@@ -8,7 +8,7 @@ export class IPRepository extends Repository<IP> {
     async check(user: User, ip: string){
         const myip = await this
         .createQueryBuilder('ip') // myEntity는 테이블의 별칭입니다.
-        .where('ip.user = :user', { user }) // user 조건 설정
+        .where('ip.user_id = :user_id', { user_id : user.user_id }) // user 조건 설정
         .andWhere('ip.ip = :ip', { ip }) // ip 조건 설정
         .getOne(); 
 
